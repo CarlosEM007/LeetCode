4f88ad31c91929a73969318504cef1d25237998b
Carlos <carloseduardomenegassi0@gmail.com>
2026-07-22 21:06:32 -0300

    Desafio 24 Completado

---
+++ LeetCode/2 -Medium/SwapNodesInPairs.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode._2__Medium
+{
+    public static class SwapNodesInPairs
+    {
+        // https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+        public static ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            int val1 = head.val;
+            int val2 = head.next.val;
+
+            head.val = val2;
+            head.next.val = val1;
+
+            head.next.next = SwapPairs(head.next.next);
+
+            return head;
+        }
+    }
+}
