d22b040b97d3d7d17600c22bb2c4b7452ac8b000
carlos.menegassi <carlos.menegassi@aliare.co>
2026-08-11 11:17:17 -0300

    limpei a main

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -9,37 +9,7 @@ namespace LeetCode
     {
         public static void Main(string[] args)
         {
-            int teste = IntegerReverse.Reverse(-123);
 
-
-            //string teste1 = ZigzagConversion.Convert("AB", 1);
-            //string teste2 = ZigzagConversion.Convert("PAYPALISHIRING", 4);
-
-            Console.WriteLine(teste);
-            //Console.WriteLine(teste2);
-
-            //ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-            //ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-
-            //ListNode resultado = MergeTwoSortedLists.MergeTwoLists(list1, list2);
-
-            //Console.WriteLine(resultado);
-
-            // Console.WriteLine("[{0}]", string.Join(", ", ConcatenationOfArray.GetConcatenation([1, 3, 2, 1])));
-            // Console.WriteLine("[{0}]", string.Join(", ", ShuffleTheArray.Shuffle([2, 5, 1, 3, 4, 7], 3)));
-            // Console.WriteLine(MaxConsecutiveOnes.FindMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]));
-            // Console.WriteLine(JumpGame.CanJump([5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]));
-
-            // Console.WriteLine("[{0}]", string.Join(", ", TransformedArray.ConstructTransformedArray([-10])));
-            // Console.WriteLine("[{0}]", string.Join(", ", TransformedArray.ConstructTransformedArray([-10, -10])));
-
-            // Console.WriteLine(TrionicArray.IsTrionic([1, 3, 5, 4, 2, 6]));
-
-            // Console.WriteLine(LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring("abcdabc"));
-
-            // Console.WriteLine(MedianOfTwoSortedArrays.FindMedianSortedArrays([1, 2], [3, 4]));
-
-            // Console.WriteLine(StringToIntegerAtoi.MyAtoi(" b11228552307"));
         }
     }
 }
