11f4935db40ace23d362d16cf64a54bf4699174d
carlos.menegassi <carlos.menegassi@aliare.co>
2026-08-19 08:24:03 -0300

    feat: Concluído desafio, 0ms.

--- LeetCode/Easy/SearchInsertPosition.cs
+++ LeetCode/Easy/SearchInsertPosition.cs
@@ -1,6 +1,40 @@
 ﻿namespace LeetCode.Easy
 {
-    public class SearchInsertPosition
+    public static class SearchInsertPosition
     {
+        // https://leetcode.com/problems/search-insert-position/submissions/2112607263/
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            if (target > nums[nums.Length - 1]) return nums.Length;
+            else if (target <= nums[0] || nums.Length == 1) return 0;
+
+            int inicio = 0;
+            int fim = nums.Length - 1;
+
+            while (inicio <= fim)
+            {
+                int indice = ((fim - inicio) / 2) + inicio;
+
+                if (nums[indice] == target)
+                {
+                    return indice;
+                }
+                else if (indice > 0 && (nums[indice - 1] < target) && (target < nums[indice]))
+                {
+                    return indice;
+                }
+                else if (nums[indice] > target)
+                {
+                    fim = indice;
+                }
+                else if (nums[indice] < target)
+                {
+                    inicio = indice + 1;
+                }
+            }
+
+            return -1;
+        }
     }
 }
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,7 +6,7 @@ namespace LeetCode
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(IndiceFirstOccurrence.StrStr("hello", "ll"));
+            Console.WriteLine(SearchInsertPosition.SearchInsert([1, 3, 5, 6], 4));
         }
     }
 }
