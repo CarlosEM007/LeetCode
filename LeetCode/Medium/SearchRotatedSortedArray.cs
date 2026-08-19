d71bf0669db7f66f26c516da9855db5138cf1eb4
carlos.menegassi <carlos.menegassi@aliare.co>
2026-08-19 13:20:02 -0300

    feat: Desafio concluido, 0ms, O(log n)

---
+++ LeetCode/Medium/SearchRotatedSortedArray.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Medium
+{
+    public static class SearchRotatedSortedArray
+    {
+        // https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/2112925465/
+
+        public static int Search(int[] nums, int target)
+        {
+            int inicio = 0;
+            int final = nums.Length - 1;
+
+            while(inicio <= final)
+            {
+                int meio = (final + inicio) / 2;
+
+                if (nums[meio] == target)
+                {
+                    return meio;
+                }
+                else if(target <= nums[final])
+                {
+                    if (target < nums[meio] && nums[meio] < nums[final])
+                    {
+                        final = meio;
+                    }
+                    else
+                    {
+                        inicio = meio + 1;
+                    }
+                }
+                else
+                {
+                    if(target < nums[meio] || nums[meio] < nums[final])
+                    {
+                        final = meio - 1;
+                    }
+                    else
+                    {
+                        inicio = meio + 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCode.Easy;
+using LeetCode.Medium;
 
 namespace LeetCode
 {
@@ -6,7 +7,35 @@ namespace LeetCode
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(SearchInsertPosition.SearchInsert([1, 3, 5, 6], 4));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 8, 1, 2, 3], 8));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 8, 1, 2, 3], 4));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 8, 1, 2, 3], 5));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 8, 1, 2, 3], 6));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 8, 1, 2, 3], 7));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 8, 1, 2, 3], 1));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 8, 1, 2, 3], 2));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 8, 1, 2, 3], 3));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 8, 1, 2, 3], 9));
+            Console.WriteLine(SearchRotatedSortedArray.Search([3, 5, 1], 0));
+            Console.WriteLine(SearchRotatedSortedArray.Search([3, 1], 0));
+            Console.WriteLine(SearchRotatedSortedArray.Search([1, 3], 3));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7], 2));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7], 4));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7], 5));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7], 6));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7], 7));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7], 8));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 0, 1, 2], 2));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 0, 1, 2], 1));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 0, 1, 2], 0));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 0, 1, 2], 7));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 0, 1, 2], 6));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 0, 1, 2], 5));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 0, 1, 2], 4));
+            Console.WriteLine(SearchRotatedSortedArray.Search([4, 5, 6, 7, 0, 1, 2], 4));
+            Console.WriteLine(SearchRotatedSortedArray.Search([1], 0));
+            Console.WriteLine(SearchRotatedSortedArray.Search([1], 1));
+            Console.WriteLine(SearchRotatedSortedArray.Search([1], 2));
         }
     }
 }
